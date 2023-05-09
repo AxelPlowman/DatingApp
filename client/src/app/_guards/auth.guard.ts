735b5b4d8b5a5f1166a27435479bb21fedf290b3
Axel Plowman <15363231+AxelPlowman@users.noreply.github.com>
2023-05-09 14:35:51 +0200

    End of section 6

---
+++ client/src/app/_guards/auth.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable, map } from 'rxjs';
+import { AccountService } from '../_service/account.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
+
+  canActivate(): Observable<boolean> {
+    return this.accountService.currentUser$.pipe(
+      map(user => {
+        if (user) return true;
+        else {
+          this.toastr.error('You shall not pass! (or something more polite)');
+          return false;
+        }
+      })
+    )
+  }
+
+}
