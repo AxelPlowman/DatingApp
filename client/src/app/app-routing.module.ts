3bfd610c187b56217a794ffadc4233757290e825
Axel Plowman <15363231+AxelPlowman@users.noreply.github.com>
2023-05-15 15:06:37 +0200

    End of section 9

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     children: [ // these children (below) will be protected by the guards in the canActivate array (above)
       { path: "members", component: MemberListComponent },
-      { path: "members/:id", component: MemberDetailComponent },
+      { path: "members/:username", component: MemberDetailComponent },
       { path: "lists", component: ListsComponent },
       { path: "messages", component: MessagesComponent },
     ]
